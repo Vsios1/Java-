f096c0608f8393befab3365740b2624f102f2091
Victor Rios <vrios.01@outlook.com>
2025-08-27 21:19:09 -0400

    operacion 1

---
+++ Java_Cero/Operecion_1/Operacion.java
@@ -0,0 +1,17 @@
+public class Operacion {
+    public static void main(String args[]){
+        int num_uno = 3; 
+        int num_dos = 45;
+        double resultado = 0;
+
+        resultado = num_uno + num_dos / 7; 
+        System.out.println("El resultado incorrecto es: "+ resultado); 
+
+ 
+        resultado = (num_uno + num_dos)/ 7; 
+        System.out.println("El resultado correcto es: "+ resultado);
+
+
+    }
+    
+}
