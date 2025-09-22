b9df4b40a0711b9d29da39ee79f1b74a09025cb1
Victor Rios <vrios.01@outlook.com>
2025-09-22 17:07:42 -0400

    Avanze de la Calculadora Universal

--- Java_Cero/Calculadora_Cientifica/C_C.java
+++ Java_Cero/Calculadora_Cientifica/C_C.java
@@ -4,11 +4,28 @@ public class C_C {
 
     public static void main(String args[]) {
 
-        Scanner c.c = new Scanner(System.in);
+        Scanner cc = new Scanner(System.in);
         int val_uno = 0, val_dos = 0, Proc = 0; 
 
         System.out.println("=====================================");
         System.out.println("Bienvenido a la Calculadora Universal");
-        
+        System.out.println("=====================================");
+
+        System.out.println("Ingresa el primer valor: ");
+        val_uno = cc.nextInt();
+
+        System.out.println("Ingresa el segundo valor: ");
+        val_dos = cc.nextInt();
+
+        System.out.println("==============================================");
+        System.out.println("Elige el tipo de operacion que deseas realizar");
+        System.out.println("==============================================");
+        System.out.println("1. Operaciones Basica");
+        System.out.println("2. Operaciones Avanzada");
+        System.out.println("3. Funciones Trigonometricas");
+        System.out.println("Elegi una opción: ");
+        System.out.println("==============================================");
+
+        int Opc = cc.nextInt();
     }
 }
