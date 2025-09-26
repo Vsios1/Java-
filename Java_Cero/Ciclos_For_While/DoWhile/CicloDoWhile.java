b926a5ea3de2c5fcc95a3af9979ff2f0048c3631
Vsios1 <Vrios.01@outlook.com>
2025-09-26 19:30:13 -0400

    Correccion de codigo

--- Java_Cero/Ciclos_For_While/DoWhile/CicloDoWhile.java
+++ Java_Cero/Ciclos_For_While/DoWhile/CicloDoWhile.java
@@ -5,10 +5,11 @@ public class CicloDoWhile {
         int num_1 = 20, num_2 = 14, resultado = 0;
 
         do {
-
-            resultado = num_1 * num_2;
             
-            System.out.print("Tu resultado es: " + resultado);
-        } while(resultado < 500);  
+            System.out.print(" Tu resultado es: " + resultado);
+            resultado = num_1 * num_2;
+            num_1++;
+            num_2++;
+        } while(resultado <= 500);  
     }
 }
